097196ec2ef6316ac434b79e3aa3a5b28d9f0816
parashac <parashacharya139@gmail.com>
2025-07-09 18:57:33 +0545

    Initial commit: Django ToDo app with auth, views, templates

--- TODO/urls.py
+++ TODO/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from django.urls import path
 from django.contrib import admin
 
+
+
 from todo_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
