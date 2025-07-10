0c5e60aa6ffeabdc342ff1b6c19f3f5e85f007d5
parashac <parashacharya139@gmail.com>
2025-07-10 18:18:37 +0545

    static/templates

--- TODO/urls.py
+++ TODO/urls.py
@@ -1,8 +1,8 @@
 """
 URL configuration for TODO project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+The urlpatterns list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include,path
-from django.contrib import admin
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("todo_app.urls")),
