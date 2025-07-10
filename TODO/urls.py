85583c1ffd1c9b833814a8f9543864c920797832
parashac <parashacharya139@gmail.com>
2025-07-10 17:25:24 +0545

    static/templates

--- TODO/urls.py
+++ TODO/urls.py
@@ -24,8 +24,8 @@ from django.contrib import admin
 from todo_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.todo_list),
-    path("delete/<int:id>/", views.todo_delete),
-    path("create/", views.todo_create),
-    path("update/<int:id>/", views.todo_update),
+    path("", views.todo_list, name='todo-list'),
+    path("delete/<int:id>/", views.todo_delete, name='todo-delete'),
+    path("create/", views.todo_create, name='todo-create'),
+    path("update/<int:id>/", views.todo_update, name='todo-update'),
 ]
